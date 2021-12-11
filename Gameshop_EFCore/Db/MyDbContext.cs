0d90de0d38ff967167c2de151a1a4b374eb201ac
Tvrtko Begović <tvrtko@adamant.hr>
2021-12-11 17:02:43 +0100

    efcore - dovršeno

---
+++ Gameshop_EFCore/Db/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gameshop_EFCore.Db
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>().ToTable("Game");
+            modelBuilder.Entity<Genre>().ToTable("Genre");
+            modelBuilder.Entity<Company>().ToTable("Company");
+        }
+    }
+}
